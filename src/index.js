8dfa2b8ae085efd68818ad6d34c8e9b216ce5951
Gustavo Tavares <gustavo.tavares2@live.com>
2021-04-17 22:36:00 -0300

    exporting all

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-export { start } from './main.js'
+export { default as State } from './State.js';
+export { default as Vector } from './Vector.js';
+export { default as GameObject } from './GameObject.js';
+export { default as GameScreen } from './Screen.js';
+export { default as Keyboard, keyCodes } from './Keyboard.js';
+export { default as Text } from './Text.js';
+export { start } from './main.js';
